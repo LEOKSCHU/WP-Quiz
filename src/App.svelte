78236751065993852f9ddd43ad43ddaf39ac1ff7
Leo Kim <me@leok.kr>
2024-06-03 12:47:49 +0900

    chore: Update project configuration and dependencies, add Spinner component to Home page, and update footer in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,19 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Router from "svelte-spa-router";
+  import { Footer, Spinner } from "flowbite-svelte";
+  import Home from "./routes/Home.svelte";
+  const routes = {
+    "/": Home,
+    //"/quiz/:quizId": Quiz,
+    //"/edit/:quizId": QuizEdit,
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p class="text-3xl">
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Router {routes} />
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<Footer footerType="default" class="fixed bottom-0 left-0 w-full z-20 justify-between">
+  <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> 웹프로그래밍 과제 </span>
+  <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> 20243555 김동현 </span>
+</Footer>
